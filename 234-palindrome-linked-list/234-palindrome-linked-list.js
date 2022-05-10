1e7891cd1628e224f8d5c7d4e7702f10b75d8d05
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-10 00:00:42 -0400

    Time: 239 ms (28.92%), Space: 77.3 MB (51.44%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    var arrNode = [];
+    let currentNode = head;
+    while(currentNode){
+        arrNode.push(currentNode['val']);
+        if(currentNode.next){
+            currentNode = currentNode.next;
+        } else{
+            currentNode = null;
+        }
+    }
+    
+    let testArr = [...arrNode];
+    arrNode.reverse();
+    
+    let arrNodeToString = arrNode.toString();
+    let testArrToString = testArr.toString();
+    
+    if( arrNodeToString === testArrToString){
+        return true;
+    } else {
+        return false;
+    }
+};

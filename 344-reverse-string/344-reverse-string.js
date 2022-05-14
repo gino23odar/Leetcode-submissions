b067147f31d56639e964d0eb55eaa04b998ae538
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-14 07:25:23 -0400

    Time: 138 ms (33.59%), Space: 49.8 MB (17.57%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s = s.reverse();
+    
+    /* why would I do this?
+    let sLength = s.length - 1;
+    let i = 0;
+    while(i < sLength/2){
+        let temp = s[i];
+        s[i] = s[sLength -i];
+        s[sLength - i] = temp;
+        i++;
+    }
+    */
+};

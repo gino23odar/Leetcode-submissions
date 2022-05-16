4aa8b586c2938c6b21b7c7222ce6ba0c4b2e9c75
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-16 19:48:24 -0400

    Time: 94 ms (80.46%), Space: 51 MB (45.82%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    if ([...(new Set(nums))].length === nums.length) return false
+    return true
+};

d92ed14ae835c23afd1f0cb6e656e54147855571
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-19 15:26:01 -0400

    Time: 87 ms (31.45%), Space: 42.5 MB (5.95%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let sol = [[1]];
+    
+    while(numRows > 1){
+        let previous = sol[sol.length -1];
+        console.log(previous);
+        let current = [1];
+        for(let i = 1; i < previous.length; i++){
+            current.push(previous[i -1] + previous[i]);
+        }
+        current.push(1);
+        sol.push(current);
+        numRows--;
+    }
+    return sol;
+};

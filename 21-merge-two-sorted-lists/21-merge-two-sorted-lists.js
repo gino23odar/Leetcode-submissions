404b6a5c5fdbbea2640dac8a461df160e75b47dc
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-20 13:39:07 -0400

    Time: 109 ms (25.18%), Space: 44 MB (74.52%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,24 +11,16 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let newList = new ListNode();
-    let headNewList = newList;
+    if(list1 === null) return list2;
+    if(list2 === null) return list1;
     
-    while(list1 !== null && list2 !== null){
-        if(list1.val < list2.val){
-            newList.next = list1;
-            list1 = list1.next;
-        }else{
-            newList.next = list2;
-            list2 = list2.next;
-        }
-        newList = newList.next;
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }
+    else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
     
-    if(list1 == null){
-        newList.next = list2;
-    } else {
-        newList.next = list1;
-    }
-    return headNewList.next;
 };

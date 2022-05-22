22c1b08e7e1c11b5adcb557f2e0dd407f532d1eb
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-22 02:33:39 -0400

    Time: 118 ms (13.25%), Space: 42.7 MB (63.67%) - LeetHub

---
+++ 120-triangle/120-triangle.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    for (let row = triangle.length - 2; row > -1; row--){
+        for (let col = 0; col < triangle[row].length; col++){
+            triangle[row][col] += Math.min(triangle[row+1][col], triangle[row+1][col+1]);
+        } 
+    } 
+    return triangle[0][0];
+};

483628d84ad6655c9b89b35f8e81f712dc3b21ef
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-22 14:55:46 -0400

    Time: 143 ms (10.03%), Space: 47.3 MB (12.24%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.js
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.js
@@ -11,21 +11,11 @@
  * @return {ListNode}
  */
 var removeElements = function(head, val) {
-    const copy = {
-        next: head,
+    if (head === null) {
+        return head;
     }
+
+    head.next = removeElements(head.next, val)
+    return head.val == val ? head.next : head;
     
-    let current = copy;
-    
-    while(current && current.next){
-        let next = current.next;
-        if(next.val === val){
-            current.next = next.next;
-            next = next.next;
-        }
-        if(!next || next.val !== val){
-            current = next;
-        }
-    }
-    return copy.next;
 };

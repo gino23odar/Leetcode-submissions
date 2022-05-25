0b2231f622ca3c79f7def000054ade4c01737a4f
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-05-25 03:00:33 -0400

    Time: 76 ms (52.62%), Space: 42.1 MB (73.71%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let ans = [], stack = [];
+    
+    while(root!== null || stack.length !== 0){
+        while(root !== null){
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        ans.push(root.val);
+        root = root.right;
+    }
+    return ans;
+};

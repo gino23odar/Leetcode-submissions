f73a3d2e1b8ba7fcce4fe007e07e6940f3e3f0b2
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-02 14:37:48 -0400

    Time: 112 ms (24.38%), Space: 44.4 MB (73.44%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -3,14 +3,13 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-    let m = matrix.length, n = matrix[0].length;
-    let output = [];
-    for(let i = 0; i < n; i++){
-        let temp = [];
-        for(let j = 0; j < m; j++){
-          temp.push(matrix[j][i]);  
+    let out = []
+    for(var i = 0; i < matrix[0].length; i++){
+        let tmp = []
+        for(var j = 0; j < matrix.length; j++){
+          tmp.push(matrix[j][i])
         }
-        output.push(temp);
+        out.push(tmp)
     }
-    return output;
+    return out
 };

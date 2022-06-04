275cd4135f3aad5a9cd3d0b5e946553a57988662
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-03 23:01:58 -0400

    Time: 86 ms (87.97%), Space: 44.7 MB (88.83%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,28 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let counter = 0;
+    
+    const dfs = (i, j) =>{
+        if(i >= 0 && j >= 0 && i < grid.length && j < grid[i].length && grid[i][j] === '1'){
+            grid[i][j] = '0';
+            dfs(i + 1, j);
+            dfs(i, j + 1);
+            dfs(i - 1, j);
+            dfs(i, j - 1);
+        }
+    }
+    
+    for(let i = 0; i < grid.length; i++){
+        for(let j = 0; j < grid[i].length; j++){
+            if(grid[i][j] === '1'){
+                counter++;
+                dfs(i, j);
+            }
+        }
+    }
+    return counter;
+};
+

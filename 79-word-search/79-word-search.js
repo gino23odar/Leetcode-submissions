173fe42c46b624a567a23b23b8b6d4108f17ea84
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-09 15:19:51 -0400

    Time: 383 ms (92.50%), Space: 42.4 MB (89.78%) - LeetHub

---
+++ 79-word-search/79-word-search.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    let rowLen = board.length, colLen = (board[0] || []).length, wordLen = word.length;
+    
+    if(!rowLen || !colLen || !wordLen) return false;
+    
+    for(let i = 0; i < rowLen; i++){
+        for(let j = 0; j < colLen; j++){
+            if(helper(board, word, i, j, 0)) return true;
+        }
+    }
+    return false;
+};
+
+const helper = (board, word, i, j, n) =>{
+    if(n === word.length) return true;
+    if(i < 0 || i >= board.length) return false;
+    if(j < 0 || j >= board[i].length) return false;
+    if(board[i][j] !== word[n]) return false;
+    
+    let ans = false;
+    let char = board[i][j];
+    
+    board[i][j] = '-';
+    
+    ans = helper(board, word, i-1, j, n+1) || helper(board, word, i+1, j, n+1) || helper(board, word, i, j-1, n+1) || helper(board, word, i, j+1, n+1);
+    
+    if(!ans) board[i][j] = char;
+    
+    return ans;
+}

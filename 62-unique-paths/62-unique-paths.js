effe23b629d44a91b77adcdd825c070b23f1a6a5
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-10 20:09:58 -0400

    Time: 103 ms (18.42%), Space: 42.3 MB (44.70%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const dp = new Array(n + 1).fill(1);
+
+    for(let row = m - 1; row > 0; row--){
+        for(let col = n - 1; col > 0; col--){
+            dp[col] = dp[col] + dp[col + 1];
+        }
+    }
+    return dp[1];
+};

ab32a22be94b3d3d61adfa945a81f73d34bb6d11
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-13 05:11:47 -0400

    Time: 88 ms (55.54%), Space: 44.3 MB (31.07%) - LeetHub

---
+++ 139-word-break/139-word-break.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    let table = new Array(s.length + 1).fill(false);
+    table[0] = true;
+    for(let i = 0; i < table.length; i++){
+        if(table[i] === false) continue;
+        for(let j = i+1; j < table.length; j++){
+            if(wordDict.includes(s.slice(i,j))) table[j] = true;
+        }
+    }
+    return table[table.length - 1];
+};

52841770db82db3759c29d6738e7149c81cbf325
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-16 05:42:55 -0400

    Time: 77 ms (95.78%), Space: 51.1 MB (97.00%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -15,5 +15,7 @@ var lowestCommonAncestor = function(root, p, q) {
     if(root === null || root === p || root === q) return root;
     let left = lowestCommonAncestor(root.left, p, q);
     let right = lowestCommonAncestor(root.right, p, q);
-    return left === null ?right :(right === null ?left :root);
+    if(!left) return right;
+    if(!right) return left;
+    return root;
 };

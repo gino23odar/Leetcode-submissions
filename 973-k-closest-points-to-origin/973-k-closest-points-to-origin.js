d584d80381b901ba1e26bc7c8e5575e8866f770c
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-06-19 14:26:55 -0400

    Time: 270 ms (70.78%), Space: 59.7 MB (73.30%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -4,32 +4,10 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    const queue = [];
-    for(let i = 0; i < points.length; i++){
-        let area = getSize(points[i]);
-        if(queue.length < k){
-            queue.push([area, points[i]]);
-            continue;
-        }
-        //console.log(queue);
-        maxHeap(queue);
-        if(area <= queue[0][0]){
-            queue.shift();
-            queue.push([area, points[i]]);
-        }
-    }
-    //console.log(queue);
-    return queue.reduce((acc, res) =>{
-        acc.push(res[1]);
-        return acc;
-    }, []);
+    const copy = [...points];
+    copy.sort((a, b) => {
+        return (a[0]**2 + a[1]**2) - (b[0]**2 + b[1]**2);
+    });
+    return copy.slice(0,k);
 };
 
-const maxHeap = (arr) =>{
-    arr.sort((a,b) => b[0] - a[0]);
-}
-
-const getSize = (el) =>{
-    const [x,y] = el;
-    return x*x + y*y;
-}

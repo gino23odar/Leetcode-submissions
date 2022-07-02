2c8f08efd1d4405caefee426f38cf116381d6c9a
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-07-02 08:11:22 -0400

    Time: 102 ms (35.10%), Space: 42.4 MB (62.53%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,12 +3,11 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if(!strs.length) return '';
-    
-    for(let i = 0; i <= strs[0].length; i++){
-        for(let j = 0; j < strs.length; j++){
-            if(strs[0][i] !== strs[j][i]) return strs[0].slice(0,i);
+    let prefix = strs[0];
+    for ( let i = 1; i < strs.length; i++ ){
+        while ( strs[i].indexOf(prefix) !== 0 ) {
+            prefix = prefix.substring(0, prefix.length - 1);
         }
     }
-    return strs[0];
+    return prefix;
 };

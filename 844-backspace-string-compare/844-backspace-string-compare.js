eccc4043b7aee85c3727380bccec987743fb0dbf
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-07-18 00:03:38 -0400

    Time: 122 ms (11.20%), Space: 44.2 MB (12.34%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.js
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -4,21 +4,12 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-    return red(s) === red(t);
+    const type = s => {
+    	const ans = [];
+    	for(let x of s.split('')) {
+        	x === '#' ? ans.pop() : ans.push(x);
+    	}
+    	return ans.join('');
+	}
+	return type(s) === type(t);
 };
-
-const red = (str) => {
-    if(str === null) return '';
-    for(let i = 0; i < str.length; i++){
-        if(str[i] === '#'){
-            if(i > 0){
-                str = str.slice(0, i - 1) + str.slice(i+1);
-                i -= 2;
-            } else {
-                str = str.slice(1);
-                i--;
-            }
-        }
-    }
-    return str;
-}

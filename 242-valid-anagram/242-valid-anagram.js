9ea4cfaf36ba596b997dc1b793a118c193ec14f0
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-07-28 00:05:40 -0400

    Time: 94 ms (83.30%), Space: 43.7 MB (63.18%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,18 +4,23 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(t.length < s.length) return false;
-    if(s.length < t.length) return false;
+    if(s.length !== t.length) return false;
+    const map1 = new Map();
+    const map2 = new Map();
     
-    let arr1 = s.split('');
-    let arr2 = t.split('');
-    arr1.sort();
-    arr2.sort();
+    s.split('').forEach((c)=>{
+      map1[c] =  map1[c] + 1 || 1; 
+    });
     
-    for(let i = 0; i < arr1.length; i++){
-        if(arr1[i] !== arr2[i]){
-            return false
-        }
+    
+    t.split('').forEach((c)=>{
+      map2[c] =  map2[c] + 1 || 1; 
+    });
+    
+
+    for(let item in map1){
+        if(map1[item] !== map2[item]) return false;
     }
-    return true;
+    
+    return true
 };

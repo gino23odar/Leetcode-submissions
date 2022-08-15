3bc9016b36a52ac976353b10237804a3bae3af4a
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-08-14 21:31:08 -0400

    Time: 164 ms (79.61%), Space: 47.7 MB (44.31%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -3,76 +3,22 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let romanArr = s.split('');
-    let resultNum = 0;
-    let i = 0;
-    let loopLength = romanArr.length;
-    while(i < loopLength){
-        if(romanArr.includes('M')){
-            resultNum += 1000;
-            let indexM = romanArr.indexOf('M');
-            if(romanArr[indexM -1] === 'C'){
-                resultNum -= 100;
-                romanArr.shift();
-                romanArr.shift();
-            }else{
-                romanArr.shift();
-            }
-        } else if(romanArr.includes('D')){
-            resultNum += 500;
-            let indexD = romanArr.indexOf('D');
-            if(romanArr[indexD -1] === 'C'){
-                resultNum -= 100;
-                romanArr.shift();
-                romanArr.shift();
-            }else{
-                romanArr.shift();
-            }
-        }else if(romanArr.includes('C')){
-            resultNum += 100;
-            let indexC = romanArr.indexOf('C');
-            if(romanArr[indexC -1] === 'X'){
-                resultNum -= 10;
-                romanArr.shift();
-                romanArr.shift();
-            }else{
-                romanArr.shift();
-            }
-        }else if(romanArr.includes('L')){
-            resultNum += 50;
-            let indexL = romanArr.indexOf('L');
-            if(indexL != 0 && romanArr[indexL -1] === 'X'){
-                resultNum -= 10;
-                romanArr.shift();
-                romanArr.shift();
-            }else{
-                romanArr.shift();
-            }
-        }else if(romanArr.includes('X')){
-            resultNum += 10;
-            let indexX = romanArr.indexOf('X');
-            if(romanArr[indexX -1] === 'I'){
-                resultNum -= 1;
-                romanArr.shift();
-                romanArr.shift();
-            }else{
-                romanArr.shift();
-            }
-        }else if(romanArr.includes('V')){
-            resultNum += 5;
-            let indexV = romanArr.indexOf('V');
-            if(romanArr[indexV -1] === 'I'){
-                resultNum -= 1;
-                romanArr.shift();
-                romanArr.shift();
-            }else{
-                romanArr.shift();
-            }
-        }else if(romanArr.includes('I')){
-            resultNum += 1;
-            romanArr.shift();
-        }
-        i++;
-    }
-    return resultNum;
+    const symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  for (let index = 0; index < s.length; index++) {
+    s[index + 1] && symbols[s[index]] < symbols[s[index + 1]]
+      ? (result -= symbols[s[index]])
+      : (result += symbols[s[index]]);
+  }
+
+  return result;
 };

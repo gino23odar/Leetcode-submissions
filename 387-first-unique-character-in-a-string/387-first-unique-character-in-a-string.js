eef02001f00fa7d7ac3414f1779b4aa9b215933b
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-08-15 22:44:52 -0400

    Time: 119 ms (83.60%), Space: 44.8 MB (89.65%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -3,20 +3,11 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let count = {};
-    for(const letter of s){
-        if(count[letter]){
-            count[letter]++;  
-        } else {
-            count[letter] = 1;
+    for (let i=0;i<s.length;i++){
+        let char = s[i]
+        if(s.indexOf(char)==i && s.indexOf(char, i + 1) == -1){
+            return i
         }
     }
-    //no order in the object, so loop over s to find the first 1.
-    for(let i = 0; i < s.length; i++){
-        const sLetter = s[i];
-        if(count[sLetter] === 1){
-            return i;
-        }
-    }
-    return -1;
+    return -1
 };

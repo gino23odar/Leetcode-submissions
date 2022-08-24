fcdc3f78c214bb31cf6d8408ea914deeaf22aea8
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-08-24 01:00:22 -0400

    Time: 109 ms (47.86%), Space: 43.2 MB (75.08%) - LeetHub

--- 342-power-of-four/342-power-of-four.js
+++ 342-power-of-four/342-power-of-four.js
@@ -3,9 +3,8 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
-    if(n === 0) return false
-    else if(n === 1) return true
+    if(n === 1) return true
+    else if(n < 4) return false
     
-    if(!Number.isInteger(n/4)) return false
-    return isPowerOfFour(n/4)
+    return (Math.log10(n)/Math.log10(4)) % 1 === 0
 };

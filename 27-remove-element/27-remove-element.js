3a9b8f076ebfc9f8b5453bc773b553b0776d4cef
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-09-28 00:26:46 -0500

    Time: 93 ms (52.10%), Space: 42.2 MB (44.77%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  const len = nums.length;
+  for(i = 0; i < len; i++) {
+    if(i > nums.length - 1) {
+      break;
+    }
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};

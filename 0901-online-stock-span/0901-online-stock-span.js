d2b45ee5486282a1ffb677b062e97909c21fc5c1
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-11-09 02:29:12 -0500

    Time: 292 ms (87.16%), Space: 51.9 MB (95.11%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.js
@@ -0,0 +1,24 @@
+
+var StockSpanner = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function(price) {
+    let count = 1;
+
+    while (this.stack.length && price >= this.stack.at(-1).price) {
+        count += this.stack.pop().count;
+    }
+    this.stack.push({price, count})
+    return count;
+};
+
+/** 
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */

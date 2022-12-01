e4782689019d3073d460f0549a06023cfae11116
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-12-01 02:24:11 -0500

    Time: 84 ms (82.42%), Space: 42.6 MB (82.42%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -5,8 +5,7 @@
 const halvesAreAlike = s => {
   let sum = 0;
   const vowels = new Set(['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']);
-  for (let i = 0; i < s.length / 2; i++){
+  for (let i = 0; i < s.length / 2; i++)
      sum += vowels.has(s[i]) - vowels.has(s[s.length - 1 - i]); 
-  }
   return !sum;
 };

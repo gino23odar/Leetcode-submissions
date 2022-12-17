9b2239e59433f39c6a71ead8e3c1088d9dabde4a
gino23odar <43919172+gino23odar@users.noreply.github.com>
2022-12-17 00:16:20 -0500

    Time: 92 ms (78.07%), Space: 46.4 MB (21.93%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+  const stack = [];
+  
+  for (let token of tokens) {
+    if (token === '+') {
+      stack.push(stack.pop()+stack.pop());
+    } else if (token === '-') {
+      stack.push(-stack.pop()+stack.pop());
+    } else if (token === '*') {
+      stack.push(stack.pop()*stack.pop());
+    } else if (token === '/') {
+      stack.push(Math.trunc(1/stack.pop()*stack.pop()));
+    } else {
+      stack.push(parseInt(token));
+    }
+  }
+  
+  return stack[0];
+};

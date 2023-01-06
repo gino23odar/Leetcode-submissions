8f0cc1818f2817de977d7b27ec07a0c88f4f8868
Gino <43919172+gino23odar@users.noreply.github.com>
2023-01-06 14:13:36 -0500

    Time: 285 ms (55.00%), Space: 58.3 MB (6.67%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function(costs, coins) {
+    let amount = 0;
+    costs.sort((a, b) => a - b);
+    console.log(costs);
+    for(let i = 0; i < costs.length; i++){
+        if(costs[i] <= coins){
+            amount += 1;
+            coins = coins - costs[i];
+        } else {
+            break;
+        }
+    }
+    return amount;
+};

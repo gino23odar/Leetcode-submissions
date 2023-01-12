cb85aac1cb895f8bceb06ed6a4445d18b30b71ac
Gino <43919172+gino23odar@users.noreply.github.com>
2023-01-12 01:22:34 -0500

    Time: 95 ms (58.85%), Space: 44.9 MB (32.85%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    return Math.max(Math.min(parseInt(s) || 0, 2147483647), -2147483648)
+    let i=0, sign = 1, num = 0, MIN = Math.pow(-2,31), MAX = (MIN + 1) * -1;
+    s = s.trim();
+    if (s[i]=='-' || s[i]=='+') sign = s[i++]=='-'?-1:1;
+    while (s[i] && s[i].charCodeAt(0)-48 <= 9 && s[i].charCodeAt(0)-48 >= 0) {
+        num = num*10 + (s[i++].charCodeAt(0)-48);
+    }
+    num = sign*num;
+    return num<=MIN?MIN:num>=MAX?MAX:num;
 };

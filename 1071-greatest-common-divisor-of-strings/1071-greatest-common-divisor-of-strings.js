41c18d999257b66482f161664bfc88bb1d478819
Gino <43919172+gino23odar@users.noreply.github.com>
2023-02-01 03:33:43 -0500

    Time: 95 ms (33.33%), Space: 47.9 MB (15.62%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    const [smallerString, largerString] = [str1, str2].sort((a,b) => a.length - b.length);
+    for (let i = smallerString.length; i > 0; i--) {
+        const testString = smallerString.slice(0, i);
+        const correctSmaller = !smallerString.split(testString).join('').length;
+        const correctLarger = !largerString.split(testString).join('').length;
+        if (correctSmaller && correctLarger) return testString;
+    }
+    return '';
+};

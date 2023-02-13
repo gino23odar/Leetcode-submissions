61391e3820a84d3de2a96922316064234e68526a
Gino <43919172+gino23odar@users.noreply.github.com>
2023-02-13 13:07:03 -0500

    Time: 67 ms (77.46%), Space: 42.2 MB (14.96%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    let dif = high - low + 1;
+    const isEven = (n) =>{
+        return n % 2 == 0;
+    }
+    
+    if(isEven(dif) || (isEven(low) && isEven(high))){
+        return Math.floor(dif/2);
+    } else if(!isEven(low) || !isEven(high)){
+        return Math.ceil(dif/2);
+    } 
+};

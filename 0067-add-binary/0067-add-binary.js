1b345f02a3f37eb02892ed5dd255829375d9b2f9
Gino <43919172+gino23odar@users.noreply.github.com>
2023-02-14 03:22:29 -0500

    Time: 67 ms (76.76%), Space: 42 MB (93.74%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,14 +4,5 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let index1 = a.length;
-    let index2 = b.length;
-    let result = '';
-    let carry = 0;
-    while (index1 || index2) {
-        let sum = (index1 > 0 ? +a[--index1] : 0) + (index2 > 0 ? +b[--index2] : 0) + carry;
-        result = (sum % 2) + result;
-        carry = sum > 1 ? 1 : 0;
-    };
-    return carry ? carry + result : result;
+    return BigInt(BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
 };

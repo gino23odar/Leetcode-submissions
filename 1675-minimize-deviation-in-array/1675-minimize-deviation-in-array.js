624fdb5af82826b1261364475e5d6908056207e7
Gino <43919172+gino23odar@users.noreply.github.com>
2023-02-24 10:03:25 -0500

    Time: 1667 ms (50.00%), Space: 66 MB (100.00%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeviation = function(nums) {
+    let maxHeap = new MaxPriorityQueue();
+    let min = Infinity;
+    for(let num of nums){
+        if(num % 2 === 1){ 
+            num *= 2;
+        }
+        min = Math.min(min, num); 
+        maxHeap.enqueue(num); 
+    }
+    let minDeviation = Infinity;
+    while(maxHeap.size()){
+        let max = maxHeap.dequeue().element; 
+        let currDev = max-min; 
+        minDeviation = Math.min(minDeviation, currDev); 
+        if(max % 2 === 0){ 
+            max = max/2; 
+            min = Math.min(min, max); 
+            maxHeap.enqueue(max); 
+        } else {
+            break; 
+        }
+    }
+    return minDeviation;
+};

68b6eeb5f640437d407ccd69a3a9b65be499d197
Gino <43919172+gino23odar@users.noreply.github.com>
2023-05-10 00:42:40 -0500

    Time: 65 ms (24.29%), Space: 41.4 MB (94.81%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -1,20 +1,39 @@
+var fillLines = function(matrix, x, y, len, start) {
+    if (len <= 0) return
+
+    matrix[x][y] = start
+    
+    for (let i = 0; i < len - 1; i++) {
+        matrix[x][y + i] = start
+        start++
+    }
+    
+    for (let i = 0; i < len - 1; i++) {
+        matrix[x + i][y + len - 1] = start
+        start++
+    }
+    
+    for (let i = len - 1; i > 0; i--) {
+        matrix[x + len - 1][y + i] = start
+        start++
+    }
+    
+    for(let i = len - 1; i > 0; i--) {
+        matrix[x + i][y] = start
+        start++
+    }
+
+    fillLines(matrix, x + 1, y + 1, len - 2, start)
+};
+
 /**
  * @param {number} n
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
-    var x = 0, xEnd = n - 1, i = 0;
-    var y = 0, yEnd = n - 1;
-    let ans = Array(n).fill(0).map(_=>Array(n));
-    while(x <= xEnd && y <= yEnd){
-        for(var xT = x; xT <= xEnd; xT++) ans[y][xT] = ++i;
-        for(var yT = y + 1; yT <= yEnd; yT++) ans[yT][xEnd] = ++i;
-        for(var xT = xEnd - 1; xT > x; xT--) ans[yEnd][xT] = ++i;
-        for(var yT = yEnd; yT > y; yT--) ans[yT][x] = ++i;
-        x++;
-        y++;
-        xEnd--;
-        yEnd--;
-    }
-    return ans;
+    const matrix = Array(n).fill().map(() => Array(n))
+
+    fillLines(matrix, 0, 0, n, 1)
+
+    return matrix
 };

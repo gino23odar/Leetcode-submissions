5d647362b2aa5554a115f2065f4ae1f86dbe2906
Gino <43919172+gino23odar@users.noreply.github.com>
2023-06-09 06:39:50 -0500

    Time: 68 ms (32.17%), Space: 43.6 MB (76.84%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -5,7 +5,7 @@
  */
 var nextGreatestLetter = function(letters, target) {
     let st = 0, end = letters.length - 1;
-    if(letters[end] < target) return letters[0];
+    if(letters[end] <= target) return letters[0];
     if(letters[st] > target) return letters[0];
     while(st <= end){
         let mid = Math.trunc((st + end) / 2);
@@ -15,9 +15,5 @@ var nextGreatestLetter = function(letters, target) {
             end = mid - 1
         }
     }
-    if(st > letters.length - 1){
-        return letters[0]
-    }
-
     return letters[st]
 };

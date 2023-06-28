f31bb835b718e1c16a8eabae7b0469339669c7ff
Gino <43919172+gino23odar@users.noreply.github.com>
2023-06-28 10:01:45 -0500

    Time: 98 ms (96.23%), Space: 54.3 MB (88.68%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.js
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.js
@@ -7,37 +7,26 @@
  * @return {number}
  */
 var maxProbability = function(n, edges, succProb, start, end) {
-    let graph = new Map()
+    const dp = new Array(n).fill(0);
+    dp[start] = 1.0;
 
-    for (let i = 0; i < edges.length; i++){
-        let [edge1, edge2] = edges[i]
-        if (!graph.has(edge1)){
-            graph.set(edge1, [])
-        }
-        if (!graph.has(edge2)){
-            graph.set(edge2, [])
-        }
-        graph.get(edge1).push([edge2, succProb[i]])
-        graph.get(edge2).push([edge1, succProb[i]])
-    }
-    
-    let queue = [[1.000,start]]
-    let visited = new Set()
-    while (queue.length){
-        let [prob, node] = queue.shift()
-        if (visited.has(node)) continue 
-        visited.add(node)
-        if (node === end){
-            return prob
-        }
-        if (graph.has(node)){
-            for (let [nextNode,nextProb] of graph.get(node)){
-                if (!visited.has(nextNode)){
-                    queue.push([nextProb*prob, nextNode])
-                }
+    for (let i = 0; i < n - 1; i++) {
+        let hasUpdate = false;
+        for (let j = 0; j < edges.length; j++) {
+            const [u, v] = edges[j];
+            const prob = succProb[j];
+            if (dp[u] * prob > dp[v]) {
+                dp[v] = dp[u] * prob;
+                hasUpdate = true;
+            }
+            if (dp[v] * prob > dp[u]) {
+                dp[u] = dp[v] * prob;
+                hasUpdate = true;
             }
         }
-        queue.sort((a,b) => b[0]-a[0])
+        if (!hasUpdate) {
+            break;
+        }
     }
-    return 0
+    return dp[end];
 };

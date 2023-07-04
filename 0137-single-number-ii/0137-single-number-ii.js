c2ca272dc20cac4d83a8ef4e9010fc3790348788
Gino <43919172+gino23odar@users.noreply.github.com>
2023-07-04 01:23:57 -0500

    Time: 54 ms (94.66%), Space: 44 MB (45.63%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let map = new Map();
-    for(let i = 0; i < nums.length; i++){
-        if(map.get(nums[i])){
-            map[nums[i]] = map.set(nums[i], map.get(nums[i])+1)
-        } else {
-            map.set(nums[i], 1)
-        }
-    }
-    for(let [key,val] of map.entries()){
-        if (val === 1) return key;
-    }
+    let ones = 0;
+    let twos = 0;
+
+    for (let num of nums) {
+        ones = (ones ^ num) & ~twos;
+        twos = (twos ^ num) & ~ones;
+    }    
+    
+    return ones;
 };

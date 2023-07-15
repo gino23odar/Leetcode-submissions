e80e53909f8c95edc8560c47164a59b3f21bde84
Gino <43919172+gino23odar@users.noreply.github.com>
2023-07-15 07:31:42 -0500

    Time: 163 ms (100.00%), Space: 94.6 MB (20.00%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} events
+ * @param {number} k
+ * @return {number}
+ */
+var maxValue = function(events, k) {
+    const dp = [];
+    events.sort((a,b)=>a[0]===b[0]?a[1]-b[1]:a[0]-b[0])
+    for(let i = 0; i < events.length; ++i){
+        dp[i] = new Array(k+1).fill(-1);
+    }
+    return f(0,k,events,dp);
+};
+
+var f = function(ind,k,events,dp){
+    if(ind == events.length || k == 0) return 0;
+    
+    if(dp[ind][k] != -1) return dp[ind][k];
+    let take = events[ind][2];
+    for(let i = ind+1; i < events.length; ++i){
+        if(events[ind][1] < events[i][0]){
+            take += f(i,k-1,events,dp);
+            break;
+        }
+    }
+    let notTake = f(ind+1,k,events,dp);
+    return dp[ind][k] = Math.max(take,notTake);
+}

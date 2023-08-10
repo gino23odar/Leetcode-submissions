1dd7f736f560c3d8bd563aae465888b4cfa98c18
Gino <43919172+gino23odar@users.noreply.github.com>
2023-08-10 16:12:34 -0500

    Time: 54 ms (77.85%), Space: 43.4 MB (14.54%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    if(nums.indexOf(target) != -1){
+        return true
+    }
+    return false
+};

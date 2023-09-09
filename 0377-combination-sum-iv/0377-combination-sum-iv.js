6415943d086e6c6ecc48b3e11ff067b537ca23ad
Gino <43919172+gino23odar@users.noreply.github.com>
2023-09-09 12:30:05 -0500

    Time: 63 ms (37.50%), Space: 41.5 MB (98.40%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+    const dp = [];
+    nums.sort((n1, n2) => n1 - n2);
+    nums.forEach(n => {dp[n] = 1;});
+    for(let i = 1; i <= target; ++i){
+        let count = 0;
+        for(let j = 0; j < nums.length && nums[j] < i; ++j){
+            count += dp[i - nums[j]];
+        }
+        dp[i] = dp[i] ? dp[i] + count : count;
+    }
+    return dp[target];
+};

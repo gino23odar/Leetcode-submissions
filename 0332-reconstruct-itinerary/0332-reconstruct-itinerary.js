929d30a20867bcb8f21d4f69bcea36264658ea85
Gino <43919172+gino23odar@users.noreply.github.com>
2023-09-14 13:43:33 -0500

    Time: 76 ms (60.85%), Space: 45.7 MB (83.72%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.js
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.js
@@ -3,32 +3,27 @@
  * @return {string[]}
  */
 var findItinerary = function(tickets) {
-    tickets.sort((t1, t2) => t1[1].localeCompare(t2[1]));
-    const adjList = new Map();
-    for (let ticket of tickets) {
-        const [src, dest] = ticket;
-        if (!adjList.has(src)) adjList.set(src, []);
-        adjList.get(src).push(dest);
-    }
-
-    const res = ["JFK"];
-    function dfs(src) {
-        if (res.length === tickets.length + 1) return true;
-        if (!adjList.has(src)) return false;
-
-        const destinations = [...adjList.get(src)];
-        for (let i = 0; i < destinations.length; i++) {
-            const dest = destinations[i];
-            adjList.get(src).splice(i, 1);
-            res.push(dest);
-            if (dfs(dest)) return true;
-
-            // backtrack
-            adjList.get(src).splice(i, 0, dest);
-            res.pop();
+    var map = {};
+    var res = [];
+    for(var i=0; i<tickets.length; i++) {
+        var dep = tickets[i][0];
+        var des = tickets[i][1];
+        if(map[dep]) {
+            map[dep].push(des);
+        } else {
+            map[dep] = [des];
         }
-        return false;
     }
-    dfs("JFK");
-    return res;
-};
+    for(let loc in map) {
+        map[loc].sort();
+    }  
+    var dfs = function(node) {
+        var des = map[node];
+        while(des && des.length>0) {
+            dfs(des.shift());
+        }
+        res.push(node);
+    }
+    dfs('JFK');
+    return res.reverse();
+}

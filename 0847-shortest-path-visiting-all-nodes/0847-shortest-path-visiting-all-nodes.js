b586a3df187596929caefd91fe5893065703bf88
Gino <43919172+gino23odar@users.noreply.github.com>
2023-09-17 18:34:15 -0500

    Time: 68 ms (97.37%), Space: 49 MB (57.89%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} graph
+ * @return {number}
+ */
+const initialize2DArray = (n, m) => { let d = []; for (let i = 0; i < n; i++) { let t = Array(m).fill(0); d.push(t); } return d; };
+
+const shortestPathLength = (g) => {
+    let n = g.length, dis = initialize2DArray(1 << n, n), q = [], res = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < n; i++) q.push([1 << i, i]);
+    while (q.length) {
+        let [mask, cur] = q.shift();
+        for (const child of g[cur]) {
+            let childMask = mask | (1 << child);
+            if (dis[childMask][child] == 0) {
+                q.push([childMask, child]);
+                dis[childMask][child] = dis[mask][cur] + 1;
+            }
+        }
+    }
+    for (let i = 0; i < n; i++) res = Math.min(res, dis[(1 << n) - 1][i]);
+    return res;
+}

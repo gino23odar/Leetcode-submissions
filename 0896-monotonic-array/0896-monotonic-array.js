1cf28e94bb0c80e37a30a6b034b3eb4dfc0ccdc5
Gino <43919172+gino23odar@users.noreply.github.com>
2023-09-29 16:30:56 -0500

    Time: 76 ms (66.44%), Space: 53.3 MB (40.30%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -3,32 +3,12 @@
  * @return {boolean}
  */
 var isMonotonic = function(nums) {
-    let dir = nums[0] - nums[nums.length - 1];
-    if(dir > 0){
-        let check = nums[0];
-        for(let i = 1; i < nums.length; i++){
-            if((check - nums[i]) >= 0){
-                check = nums[i]
-            } else {
-                return false;
-            }
-        }
-    } else if(dir == 0) {
-        const isAllEqual = nums.every((element) => element === nums[0]);
-        if(isAllEqual){
-            return true
-        } else {
-            return false
-        }
-    } else {
-        let check = nums[0];
-        for(let i = 1; i < nums.length; i++){
-            if((check - nums[i]) <= 0){
-                check = nums[i]
-            } else {
-                return false;
-            }
-        }
+    let increasing = true;
+    let decreasing = true;
+
+    for(let i = 1 ; i < nums.length ; i++){
+        if(nums[i] < nums[i - 1]) increasing = false;
+        if(nums[i] > nums[i - 1]) decreasing = false;
     }
-    return true
+    return increasing || decreasing;
 };

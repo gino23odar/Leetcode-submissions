d1d2d4d8bb43642394003aa2c6c7eb697a9ca067
Gino <43919172+gino23odar@users.noreply.github.com>
2023-10-01 10:14:00 -0500

    Time: 66 ms (51.92%), Space: 48.1 MB (71.26%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,11 +3,5 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let arr = s.split(' ');
-    for(let word in arr){
-        //let niu = arr[word].split('').reverse().join('');
-        //arr[word] = niu;
-        arr[word] = arr[word].split('').reverse().join('')
-    }
-    return arr.join(' ')
+    return s.split(' ').map(el => el.split('').reverse().join('')).join(' ');
 };

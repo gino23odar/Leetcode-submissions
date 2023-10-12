74fd80bdf19864b63009baa9279d2747570addbe
Gino <43919172+gino23odar@users.noreply.github.com>
2023-10-12 16:25:53 -0500

    Time: 56 ms (24.62%), Space: 42.6 MB (27.69%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.js
@@ -0,0 +1,53 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function MountainArray() {
+ *     @param {number} index
+ *     @return {number}
+ *     this.get = function(index) {
+ *         ...
+ *     };
+ *
+ *     @return {number}
+ *     this.length = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {number} target
+ * @param {MountainArray} mountainArr
+ * @return {number}
+ */
+var findInMountainArray = function(target, mountainArr) {
+  const len = mountainArr.length();
+
+  let peakIdx = 0;
+  let l = 0;
+  let r = len;
+  while (l < r) {
+    const m = ~~((l + r) / 2);
+    if (mountainArr.get(m) < mountainArr.get(m + 1)) l = peakIdx = m + 1;
+    else r = m;
+  }
+
+  l = 0;
+  r = peakIdx - 1;
+  while (l <= r) {
+    const m = ~~((l + r) / 2);
+    if (mountainArr.get(m) === target) return m;
+    else if (mountainArr.get(m) < target) l = m + 1;
+    else r = m - 1;
+  }
+
+  l = peakIdx;
+  r = len - 1;
+  while (l <= r) {
+    const m = ~~((l + r) / 2);
+    if (mountainArr.get(m) === target) return m;
+    else if (mountainArr.get(m) > target) l = m + 1;
+    else r = m - 1;
+  }
+  return -1;
+};

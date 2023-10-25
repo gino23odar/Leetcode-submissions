36f8d4d2f34b5456d787de90e335cf056f8c92ce
Gino <43919172+gino23odar@users.noreply.github.com>
2023-10-25 06:07:17 -0500

    Time: 50 ms (67.39%), Space: 41.5 MB (75.00%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
@@ -4,19 +4,10 @@
  * @return {number}
  */
 var kthGrammar = function(n, k) {
-    return halfit(n, k-1);
-};
-
-const halfit = (n, pos) =>{
-    if(n === 1) return '0';
-    const half = pos >> 1;
-    const prev = halfit(n-1, half);
-    
-    if(prev == '1'){
-        if(pos % 2 === 1) return 0
-        else return 1;
-    } else {
-        if(pos % 2 === 1) return 1
-        else return 0;
+    if(n == 1) return 0;
+    else {
+        let mid = Math.pow(2,n-1) /2
+        if(k <= mid) return kthGrammar(n-1, k)
+        else return kthGrammar(n-1, k-mid) === 0 ?1 :0;
     }
-}
+};

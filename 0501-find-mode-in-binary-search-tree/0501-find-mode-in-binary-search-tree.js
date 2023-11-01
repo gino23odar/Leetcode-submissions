c7097c5e784895f16249a2e5d568fead12e99814
Gino <43919172+gino23odar@users.noreply.github.com>
2023-11-01 17:31:44 -0500

    Time: 71 ms (61.34%), Space: 49.4 MB (62.61%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findMode = function(root) {
+  const map = new Map()
+  const maxMap = new Map()
+  let max = -Infinity
+  
+  function dfs(root){
+    if(!root) return
+    map.set(root.val, (map.get(root.val) || 0) + 1)
+      if(map.get(root.val) >= max){
+        max = map.get(root.val)
+        if(!maxMap.get(max)) maxMap.set(max, new Set())
+        maxMap.get(max).add(root.val)
+      }    
+    dfs(root.left)
+    dfs(root.right)
+  } 
+  
+  dfs(root)
+  return [...maxMap.get(max)]
+};

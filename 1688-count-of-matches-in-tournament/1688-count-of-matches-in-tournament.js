b9a1593919f2c2836cc415d31feff4b126c839d9
Gino <43919172+gino23odar@users.noreply.github.com>
2023-12-05 04:32:56 -0500

    Time: 49 ms (71.30%), Space: 42.3 MB (5.44%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,15 +3,5 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    let count = 0;
-    while(n > 1){
-        if(n % 2 == 0){
-            count += n/2;
-            n = n/2
-        } else {
-            count += (n-1)/2;
-            n = (n-1) / 2 + 1
-        }
-    }
-    return count
+    return n-1
 };

20c0863a607c3bf48619f47fe3d4beced2251fb7
Gino <43919172+gino23odar@users.noreply.github.com>
2023-12-31 02:30:56 -0500

    Time: 113 ms (6.67%), Space: 42.3 MB (70.00%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -3,11 +3,15 @@
  * @return {number}
  */
 var maxLengthBetweenEqualCharacters = function(s) {
+    let idx = [];
     let max = -1;
+    
     for(let i = 0; i < s.length; i++){
-        let cur = s.lastIndexOf(s[i]) - s.indexOf(s[i]) - 1;
-        if(cur > max) max = cur
-        if((s.length - i) <= max) break;
+        if(idx[s[i]] !== undefined){
+            max = Math.max(max, i - idx[s[i]] -1)
+        } else {
+            idx[s[i]] = i
+        }
     }
     return max
 };

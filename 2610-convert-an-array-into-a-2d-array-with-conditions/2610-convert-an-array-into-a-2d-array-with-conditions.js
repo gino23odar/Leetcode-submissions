60cbd2eda8d2103cd3aa04c358fecaeaba1c478d
Gino <43919172+gino23odar@users.noreply.github.com>
2024-01-02 18:04:39 -0500

    Time: 238 ms (7.37%), Space: 46.2 MB (51.58%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.js
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.js
@@ -3,30 +3,15 @@
  * @return {number[][]}
  */
 var findMatrix = function(nums) {
-    let map = new Map();
-    let res = [];
-    
-    for(let i = 0; i < nums.length; i++){
-        if(map.get(nums[i])){
-            map.set(nums[i], map.get(nums[i]) + 1);
-        } else {
-            map.set(nums[i], 1);
-        }
+    let ans=[];
+    let obj={}
+    for(let num of nums){
+        let index = obj[num]||0;
+        let arr = ans[index]||[];
+        arr.push(num);
+        ans[index] = arr;
+        obj[num] = index+1
+        //console.log(ans, obj)
     }
-    
-    const high = Math.max(...map.values());
-    
-    for(let i = 0; i < high; i++){
-        let sub = [];
-        for(const [key, value] of map){
-            if(value > 0){
-                sub.push(key);
-                map.set(key, map.get(key) - 1)
-                if(value == 0) map.delete(key)
-            }
-        }
-        res.push(sub)
-    }
-    
-    return res
+    return ans
 };

88a77cea8ca5048d768399e32f3ac68d8e1ba1c4
Gino <43919172+gino23odar@users.noreply.github.com>
2024-02-09 21:21:47 -0500

    Time: 66 ms (53.41%), Space: 50.5 MB (33.41%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    total += helper(i, i, s);
+    total += helper(i, i+1, s);
+  }
+  return total;
+};
+
+function helper(left, right, s) {
+  let count = 0;
+  while (left > -1 && right < s.length && s[left] === s[right]) {
+    count++;
+    left--;
+    right++;
+  }
+
+  return count;
+}

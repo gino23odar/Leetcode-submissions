277e639a60f3781ba7213b7e6064168d9a04a990
Gino <43919172+gino23odar@users.noreply.github.com>
2024-02-19 14:52:24 -0500

    Time: 62 ms (79.70%), Space: 52.3 MB (48.32%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,8 +3,12 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
+    if(n < 1) return false;
+    if(n < 3) return true;
     while(n > 1){
-        n /= 2
+        let r = n % 2;
+        if(r != 0) return false;
+        n /= 2;
     }
-    return n == 1
+    return true
 };

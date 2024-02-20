b2cc67a7a8adcf9a45485f9460f4eb760dd1a244
Gino <43919172+gino23odar@users.noreply.github.com>
2024-02-20 15:22:35 -0500

    Time: 54 ms (81.21%), Space: 50.4 MB (51.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let total = 0, sum = 0;
-    for(let i = 0; i < nums.length; i++){
-        sum += nums[i];
-        total += i + 1; 
+    let total = 0
+    for(let i = 0; i < nums.length; i++){ 
+        total += i + 1 - nums[i]
     }
-    return total - sum
+    return total
 };

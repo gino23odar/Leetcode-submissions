3e05072c5ef55d054679b6aa75a8274d010103f0
Gino <43919172+gino23odar@users.noreply.github.com>
2024-03-09 13:50:41 -0500

    Time: 85 ms (28.32%), Space: 66.8 MB (30.61%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let set = new Set(nums1);
+    let res = -1;
+    
+    for(let i = 0; i < nums2.length; i++){
+        if(set.has(nums2[i])){
+            res = nums2[i];
+            break;
+        }
+    }
+    return res
+};

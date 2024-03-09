e6ce1cbcb2e739964d7c3033d65795e5a0b787e1
Gino <43919172+gino23odar@users.noreply.github.com>
2024-03-09 14:35:06 -0500

    Time: 69 ms (74.09%), Space: 56.7 MB (72.02%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -4,14 +4,16 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-    let set = new Set(nums1);
-    let res = -1;
-    
-    for(let i = 0; i < nums2.length; i++){
-        if(set.has(nums2[i])){
-            res = nums2[i];
-            break;
+    let p1 = 0;
+    let p2 = 0;
+    while(p1 < nums1.length && p2 < nums2.length){
+        if(nums1[p1] == nums2[p2]) {
+            return nums1[p1]
+        }else if(nums1[p1] < nums2[p2]) {
+            p1 += 1
+        } else{
+            p2 += 1
         }
     }
-    return res
+    return -1;
 };

b5860e3c798abe226d6fbc0647886f3c8181f268
Gino <43919172+gino23odar@users.noreply.github.com>
2024-03-12 13:00:33 -0500

    Time: 65 ms (28.26%), Space: 50.5 MB (86.96%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.js
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.js
@@ -10,33 +10,24 @@
  * @return {ListNode}
  */
 var removeZeroSumSublists = function(head) {
-    let node = head, map = {}, preSum = 0;
-    while (node != null) {
-        preSum += node.val
-        //if preSum = 0, all prev values should be removed, head will point next and map/object will be cleared
-        if (preSum === 0) {
-            head = node.next;
-            map = {};
-        }
-        //if values are not zero but its in the map. i.e. it was preSum somewhere in the past. 
-        //find it from the start and till this node remove this value and assign it to next;
-        else if (map[preSum] !== undefined) {
-            let start = map[preSum], temp = preSum;
-            while (start !== node) {
-                start = start.next;
-                temp += start.val;
-                if (start === node) break;
-                delete map[temp];
-            }
-            let prev = map[preSum];
-            prev.next = node.next;
-        }
-        // it means not present anywhere and we just need it to add in the map
-        else {
-            map[preSum] = node;
-        }
-        //moving to next place
-        node = node.next;
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let p = dummy;
+    let map = new Map();
+    let pre = 0;
+    while (p !== null) {
+        pre = pre + p.val;
+        map.set(pre, p);
+        p = p.next;
     }
-    return head;
+    pre = 0;
+    p = dummy;
+    while (p !== null) {
+        pre = pre + p.val;
+        if (map.has(pre)) {
+            p.next = map.get(pre).next;
+        }
+        p = p.next;
+    }
+    return dummy.next;
 };

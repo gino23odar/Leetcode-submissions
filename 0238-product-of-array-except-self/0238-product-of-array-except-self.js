65438626981e9245824626a54a3c2b9defa4385b
Gino <43919172+gino23odar@users.noreply.github.com>
2024-03-15 11:25:53 -0500

    Time: 73 ms (96.10%), Space: 60.4 MB (61.68%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const result = [];
+    let productSoFar = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = productSoFar
+        productSoFar *= nums[i]
+    }
+
+    productSoFar = 1
+    for (let j = nums.length-1; j >= 0; j--) {
+        result[j] *= productSoFar
+        productSoFar *= nums[j]
+    }
+    return result;
+};

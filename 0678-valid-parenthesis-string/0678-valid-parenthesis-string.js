55b1f2c9b85f85a1cf26024971fab7bc4776ff60
Gino <43919172+gino23odar@users.noreply.github.com>
2024-04-08 06:09:13 -0500

    Time: 57 ms (30.34%), Space: 48.5 MB (79.40%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.js
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.js
@@ -3,31 +3,21 @@
  * @return {boolean}
  */
 var checkValidString = function(s) {
-    let stack = [];
-    let stackast = [];
-    for(let i = 0; i < s.length; i++){
-        if(s[i] == ')' && stack.length){
-            stack.pop();
-            continue;
-        } if(s[i] == ')' && stackast.length){
-            stackast.pop();
-            continue;
-        } else if(s[i] == '('){
-            stack.push(i)
-        } else if(s[i] == '*'){
-            stackast.push(i)
-        } else if(s[i] == ')'){
-            return false;
+    let open = 0;
+    let close = 0;
+    
+    for(let i = 0; i <= s.length-1; i++){
+        if(s[i] == '(' || s[i] == '*'){
+            open++
+        } else {
+            open--
         }
-    }
-    
-    if(stack.length > stackast.length) return false
-    
-    for(let i = 1; i <= stack.length; i++){
-        if(stack[stack.length-i] > stackast[stackast.length-i]){
-            return false
+        if(s[s.length-1-i] == ')' || s[s.length-1-i] == '*'){
+            close++
+        } else {
+            close--
         }
+        if(open < 0 || close < 0) return false
     }
-    
     return true
 };

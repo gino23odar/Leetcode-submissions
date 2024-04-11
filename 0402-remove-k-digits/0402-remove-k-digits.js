e574c7de5d96540020c087537159bdf981b832ba
Gino <43919172+gino23odar@users.noreply.github.com>
2024-04-11 13:54:03 -0500

    Time: 77 ms (15.84%), Space: 54.7 MB (84.82%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    if(num.length == k) return '0'
+    let st = [];
+    for(let i = 0; i < num.length; i++){
+        while(st.length > 0 && k > 0 && num[i] < st[st.length-1]){
+            st.pop();
+            k--;
+        }
+        st.push(num[i])
+    }
+    while(k > 0){
+        st.pop();
+        k--;
+    }
+    while(st.length && st[0] == 0){
+        st.shift();
+    }
+    return st.length? st.join('') : '0'
+};

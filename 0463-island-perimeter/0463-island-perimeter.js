b33073583e806e92959a7ae6b03aa5c30690a0cd
Gino <43919172+gino23odar@users.noreply.github.com>
2024-04-18 14:27:07 -0500

    Time: 138 ms (44.93%), Space: 56.9 MB (95.83%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.js
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -7,10 +7,15 @@ var islandPerimeter = function(grid) {
     for(let i = 0; i < grid.length; i++){
         for(let j = 0; j < grid[i].length; j++){
             if(grid[i][j]){
-                if(!grid[i][j+1]) count++;
-                if(!grid[i][j-1]) count++;
-                if(!grid[i-1] || !grid[i-1][j]) count++;
-                if(!grid[i+1] || !grid[i+1][j]) count++;
+                count += 4;
+                
+                if (i > 0 && grid[i - 1][j] === 1) {
+                    count -= 2;
+                }
+                
+                if (j > 0 && grid[i][j - 1] === 1) {
+                    count -= 2;
+                }
             }
         }
     }

61c1de6e61c1de92b4f88ee03d521f82d939ddda
Gino <43919172+gino23odar@users.noreply.github.com>
2024-05-02 05:22:13 -0500

    Time: 76 ms (39.13%), Space: 52.5 MB (26.52%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
@@ -4,17 +4,12 @@
  */
 var findMaxK = function(nums) {
     let sorted = nums.sort((a,b) => b - a);
-    //console.log(sorted)
     let map = new Map();
-    for(let i = 0; i < nums.length; i++){
-        map.set(nums[i], map.get(nums[i])+1 || 1);
-    }
-    //console.log(map)
     let max = -1;
     for(let i = 0; i < nums.length; i++){
+        map.set(nums[i], map.get(nums[i])+1 || 1);
         if(map.get(-nums[i])){
-            max = nums[i]
-            break
+            max = Math.abs(nums[i]);
         }
     }
     return max;

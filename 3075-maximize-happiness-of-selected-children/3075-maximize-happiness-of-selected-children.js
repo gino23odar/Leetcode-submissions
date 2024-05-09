9fe81cdcaafdd888529406d57e68df004864d796
Gino <43919172+gino23odar@users.noreply.github.com>
2024-05-09 10:07:45 -0500

    Time: 239 ms (71.69%), Space: 69.4 MB (17.12%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} happiness
+ * @param {number} k
+ * @return {number}
+ */
+var maximumHappinessSum = function(happiness, k) {
+    happiness = happiness.sort((a,b) => b - a);
+    let selected = happiness.slice(0,k);
+    let ans = 0;
+    for(let i = 0; i < k; i++){
+        if((happiness[i] - i) > 0){
+            ans += happiness[i] - i
+        }
+    }
+    //console.log(ans)
+    return ans
+};

34ed931bbb1c10125eaafd733170aa6b3570cd9a
Gino <43919172+gino23odar@users.noreply.github.com>
2024-05-11 13:09:48 -0500

    Time: 104 ms (90.91%), Space: 58.3 MB (63.64%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.js
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.js
@@ -5,109 +5,36 @@
  * @return {number}
  */
 var mincostToHireWorkers = function(quality, wage, k) {
-  const n = quality.length;
-  const tuples = new Array(n).fill();
+    let ratios = []
 
-  for (let i = 0; i < n; i++) {
-    const expectedWage = wage[i] / quality[i];
-    tuples[i] = [expectedWage, quality[i]];
-  }
+    for(let i = 0; i < quality.length; i++) {
+        ratios.push([wage[i] / quality[i], quality[i], wage[i]]);
+    } 
 
-  tuples.sort((a, b) => a[0] - b[0]);
+    ratios.sort((a, b) => a[0] - b[0]);
 
-  let ans = Infinity;
-  let sumOfQuality = 0;
-  const maxHeap = new MaxHeap([]);
+    let res = Infinity;
+    let maxHeap = new PriorityQueue({compare: (a, b) => b - a});
+    let heapSum = 0;
 
-  for (let i = 0; i < n; i++) {
-    const [expectedWage, quality] = tuples[i];
-    sumOfQuality += quality;
-    maxHeap.insert(quality);
+    for(let i = 0; i < ratios.length; i++) {
+        let ratio = ratios[i][0];
 
-    if (maxHeap.getLength() > k) {
-      sumOfQuality -= maxHeap.remove();
-    }
-    if (maxHeap.getLength() === k) {
-      ans = Math.min(ans, sumOfQuality * expectedWage);
-    }
-  }
+        let [ , qual, ] = ratios[i];
 
-  return ans;
+        maxHeap.enqueue(qual);
+        heapSum += qual;
+
+        if(maxHeap.size() > k) {
+            heapSum -= maxHeap.dequeue();
+        }
+
+        if(maxHeap.size() === k) {
+            res = Math.min(res, heapSum * ratio);
+        }
+
+    } 
+
+    return res;
 };
 
-class MaxHeap {
-  constructor(array) {
-    this.heap = this.buildHeap(array);
-  }
-
-  getLength() {
-    return this.heap.length;
-  }
-
-  buildHeap(array) {
-    const firstParentIdx = Math.floor((array.length - 2) / 2);
-    for (let i = firstParentIdx; i >= 0; i--) {
-      this.siftDown(array, i, array.length - 1);
-    }
-    return array;
-  }
-
-  peek() {
-    return this.heap[0];
-  }
-
-  siftUp(array) {
-    let idx = array.length - 1;
-    let parentIdx = Math.floor((idx - 1) / 2);
-    while (parentIdx >= 0) {
-      if (array[idx] > array[parentIdx]) {
-        this.swap(array, idx, parentIdx);
-        idx = parentIdx;
-        parentIdx = Math.floor((idx - 1) / 2);
-      } else {
-        break;
-      }
-    }
-  }
-
-  insert(num) {
-    this.heap.push(num);
-    this.siftUp(this.heap);
-  }
-
-  siftDown(array, idx, endIdx) {
-    let leftChildIdx = idx * 2 + 1;
-    while (leftChildIdx <= endIdx) {
-      let idxToSwap = leftChildIdx;
-      let rightChildIdx = idx * 2 + 2;
-      if (
-        rightChildIdx <= endIdx &&
-        array[rightChildIdx] > array[leftChildIdx]
-      ) {
-        idxToSwap = rightChildIdx;
-      }
-      if (array[idxToSwap] > array[idx]) {
-        this.swap(array, idxToSwap, idx);
-        idx = idxToSwap;
-        leftChildIdx = idx * 2 + 1;
-      } else {
-        break;
-      }
-    }
-  }
-
-  remove() {
-    const valueToRemove = this.heap[0];
-    this.swap(this.heap, 0, this.heap.length - 1);
-    this.heap.pop();
-    this.siftDown(this.heap, 0, this.heap.length - 1);
-
-    return valueToRemove;
-  }
-
-  swap(array, i, j) {
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}

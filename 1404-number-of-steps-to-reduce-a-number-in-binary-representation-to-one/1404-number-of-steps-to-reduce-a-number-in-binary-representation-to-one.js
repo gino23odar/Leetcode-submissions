da0bc65cc6a60ae8bbb3b456dbd9b81eebb9d1f5
Gino <43919172+gino23odar@users.noreply.github.com>
2024-05-29 01:27:54 -0500

    Time: 54 ms (62.96%), Space: 51.1 MB (18.52%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -2,18 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var numSteps = function(s) {
-  let steps = 0;
-  let carry = 0;
-  for (let i = s.length - 1; i > 0; i--) {
-    const sum = carry + Number(s[i]);
-    if (sum % 2 === 1) {
-      steps += 2;
-      carry = 1;
-    } else {
-      steps += 1;
-    }
-  }
+let res;
+const numSteps = (s) => {
+    res = Number.MAX_SAFE_INTEGER;
+    let cur = BigInt('0b' + s);
+    dfs(0, cur);
+    return res;
+};
 
-  return steps + carry;
+const dfs = (step, x) => {
+    if (x == 1) {
+        res = Math.min(res, step);
+        return;
+    }
+    if (x % 2n == 1) {
+        dfs(step + 1, x + 1n);
+    } else {
+        dfs(step + 1, x / 2n);
+    }
 };

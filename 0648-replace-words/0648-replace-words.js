5df96178ba61f82923299a973196f9a652a1066e
Gino <43919172+gino23odar@users.noreply.github.com>
2024-06-07 05:01:43 -0500

    Time: 78 ms (99.17%), Space: 61.9 MB (69.42%) - LeetHub

--- 0648-replace-words/0648-replace-words.js
+++ 0648-replace-words/0648-replace-words.js
@@ -4,16 +4,38 @@
  * @return {string}
  */
 var replaceWords = function(dictionary, sentence) {
-  let a = sentence.split(" ");
-  let b = dictionary;
+    const dictionarySet = new Set();
+    const sentenceArr = sentence.split(" ");
+    let minLen = Number.MAX_VALUE;
+    let maxLen = Number.MIN_VALUE;
 
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (a[i].startsWith(b[j])) {
-        a[i] = b[j];
-      }
+    dictionary.forEach(root => {
+        dictionarySet.add(root);
+
+        if(root.length < minLen) minLen = root.length;
+        if(root.length > maxLen) maxLen = root.length;
+    });
+
+    for(let i = 0; i < sentenceArr.length; i++) {
+        const word = sentenceArr[i];
+
+        if(minLen === maxLen) {
+            const sub = word.slice(0, maxLen);
+            
+            if(dictionarySet.has(sub)) {
+                sentenceArr[i] = sub;
+            }
+        } else {
+            for(let j = minLen; j <= maxLen; j++) {
+                const sub = word.slice(0, j);
+            
+                if(dictionarySet.has(sub)) {
+                    sentenceArr[i] = sub;
+                    break;
+                }
+            }
+        }
     }
-  }
 
-  return a.join(" ");
+    return sentenceArr.join(" ");
 };

65787b8b994032103cd4a136cd7330d70b2de29c
Gino <43919172+gino23odar@users.noreply.github.com>
2024-06-07 05:24:05 -0500

    Time: 75 ms (97.05%), Space: 24.9 MB (94.66%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,9 +1,15 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        words = sentence.split()
-        for i in range(len(words)):
-            for root in dictionary:
-                if not words[i].startswith(root):
-                    continue
-                words[i] = root
-        return " ".join(words)
+        d = {w: len(w) for w in dictionary}
+        mi, ma = min(d.values()), max(d.values())
+        wrds = sentence.split()
+        res = []
+        for w in wrds:
+            c = w
+            for i in range(mi, min(ma, len(w)) + 1):
+                ss = w[:i]
+                if ss in d:
+                    c = ss
+                    break
+            res.append(c)
+        return " ".join(res)

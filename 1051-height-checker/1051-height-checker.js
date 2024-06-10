d8bbf00ad3467c9302842c5bff9bb4ac95ab6c12
Gino <43919172+gino23odar@users.noreply.github.com>
2024-06-10 05:39:19 -0500

    Time: 53 ms (66.93%), Space: 49 MB (65.10%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let expected  = [...heights].sort((a,b) => a - b)
+    //console.log(expected)
+    //console.log(heights)
+    let count = 0
+    for(let i = 0; i < heights.length; i++){
+        if(expected[i] != heights[i]) count++
+    }
+    return count
+};

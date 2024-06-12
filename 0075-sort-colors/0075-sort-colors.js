09dac4710bc4840b8fbbedcdff61009aeeb5173b
Gino <43919172+gino23odar@users.noreply.github.com>
2024-06-12 09:49:57 -0500

    Time: 72 ms (5.35%), Space: 51.1 MB (5.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,19 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let c = Array(3).fill(0);
-    for(let i = 0; i < nums.length; i++){
-        c[nums[i]]++
-    }
-    //console.log(c)
-    let idx = 0;
-    for(let i = 0; i < c.length; i++){
-        let sub = c[i];
-        for(let j = 0; j < sub; j++){
-            nums[idx] = i
-            idx++
+    let isSorted = false;
+    while(!isSorted){
+        isSorted = true;
+        for(let i = 0; i < nums.length - 1; i++){
+            if(nums[i] > nums[i + 1]){
+                isSorted = false;
+                [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+            }
         }
     }
-    
-    return nums
 };

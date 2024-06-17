36d963b30cba89be01a5332cdafac7f5bbd6d4fe
Gino <43919172+gino23odar@users.noreply.github.com>
2024-06-17 13:11:06 -0500

    Time: 60 ms (54.11%), Space: 49 MB (91.10%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -3,14 +3,17 @@
  * @return {boolean}
  */
 var judgeSquareSum = function(c) {
-    if (c === 0) {
-        return true
-    }
-    for (let a = 0; a*a < c; a++) {
-        let b = Math.sqrt(parseFloat(c-a*a));
-        if (b - Math.round(b) === 0) {
+    let i = 0;
+    let j = Math.ceil(Math.sqrt(c));
+    while(i<=j){
+        let sum = i*i + j*j;
+        if(sum < c){
+            i++;
+        }else if (sum > c){
+            j--;
+        }else{
             return true;
         }
     }
-        return false
+    return false;
 };

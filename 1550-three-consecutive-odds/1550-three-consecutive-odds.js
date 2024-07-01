10aba546b0ce6b514e673b76b2c66e94c5418f1b
Gino <43919172+gino23odar@users.noreply.github.com>
2024-07-01 17:02:06 -0500

    Time: 60 ms (19.90%), Space: 49.1 MB (27.04%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    for(let i = 0; i < arr.length; i++){
+        if((arr[i]%2==1) && (arr[i+1]%2==1) && (arr[i+2]%2==1)) return true
+    }
+    return false
+};

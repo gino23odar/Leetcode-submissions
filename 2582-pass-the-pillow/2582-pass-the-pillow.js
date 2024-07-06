951ca568775fbbee4dd28f0767588020793ae36a
Gino <43919172+gino23odar@users.noreply.github.com>
2024-07-06 05:49:50 -0500

    Time: 47 ms (90.24%), Space: 48.9 MB (31.71%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.js
+++ 2582-pass-the-pillow/2582-pass-the-pillow.js
@@ -4,8 +4,12 @@
  * @return {number}
  */
 var passThePillow = function(n, time) {
-    let d = [];
-    for (let i = 1; i <= n; i++) d.push(i);
-    for (let i = n - 1; i > 1; i--) d.push(i);
-    return d[time % d.length];
+    let count = 1;
+    let direction = 1;
+    for (let i = 0; i < time; i++) {
+        if (count === 1) direction = 1;
+        if (count === n) direction = -1;
+        count = direction === 1 ? count + 1 : count - 1;
+    }
+    return count;
 };

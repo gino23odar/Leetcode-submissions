71f2e919be4547dfd661b0789dc84f6f5bded2a5
Gino <43919172+gino23odar@users.noreply.github.com>
2024-08-09 12:24:47 -0500

    Time: 52 ms (68.15%), Space: 49.2 MB (52.87%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,20 +4,14 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    if (s.length > t.length) return false;
-    let j = 0;
     let i = 0;
-    const s_list = s.split('');
-    const t_list = t.split('');
-    let isSub = [];
-    while(i < t.length) {
-        if (j >= s.length) break;
-        let item = s[j];
-        if (item === t_list[i]) {
-            isSub.push(1);
+    let j = 0;
+    
+    while(i < s.length && j < t.length){
+        if(s[i] == t[j]){
+            i++;
             j++;
-        }
-        i++;
+        } else j++
     }
-    return isSub.length === s.length;
+    return i == s.length
 };

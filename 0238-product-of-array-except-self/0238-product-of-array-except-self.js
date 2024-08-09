34085d0bbd6d3d20892a28a8d289b13688d4e1b3
Gino <43919172+gino23odar@users.noreply.github.com>
2024-08-09 14:39:22 -0500

    Time: 118 ms (24.91%), Space: 70.4 MB (5.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,17 +3,26 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const result = [];
-    let productSoFar = 1;
-    for (let i = 0; i < nums.length; i++) {
-        result[i] = productSoFar
-        productSoFar *= nums[i]
+    let pre = [nums[0]];
+    let suf = Array(nums.length).fill(nums[nums.length-1]);
+    
+    for(let i = 1; i < nums.length; i++){
+        pre[i] = pre[i-1] * nums[i]
     }
 
-    productSoFar = 1
-    for (let j = nums.length-1; j >= 0; j--) {
-        result[j] *= productSoFar
-        productSoFar *= nums[j]
+    for(let i = nums.length -2; i >= 0; i--){
+        suf[i] = suf[i+1] * nums[i]
     }
-    return result;
+
+    let ans = Array(nums.length);
+    for(let i = 0; i < nums.length; i++){
+        if(i == 0){
+            ans[i] = suf[i+1]
+        } else if(i == nums.length-1){
+            ans[i] = pre[i-1]
+        } else {
+            ans[i] = suf[i+1] * pre[i-1]
+        }
+    }
+    return ans
 };

bada4b4a491ae1e9a4a7dc01eba189efba04fe3e
Gino <43919172+gino23odar@users.noreply.github.com>
2024-08-09 14:48:12 -0500

    Time: 96 ms (81.51%), Space: 64.6 MB (45.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,26 +3,18 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let pre = [nums[0]];
-    let suf = Array(nums.length).fill(nums[nums.length-1]);
+    let n = nums.length; 
+    let ans = Array(n).fill(1);
     
-    for(let i = 1; i < nums.length; i++){
-        pre[i] = pre[i-1] * nums[i]
+    for(let i = 1; i < n; i++){
+        ans[i] = ans[i-1] * nums[i-1]
     }
-
-    for(let i = nums.length -2; i >= 0; i--){
-        suf[i] = suf[i+1] * nums[i]
-    }
-
-    let ans = Array(nums.length);
-    for(let i = 0; i < nums.length; i++){
-        if(i == 0){
-            ans[i] = suf[i+1]
-        } else if(i == nums.length-1){
-            ans[i] = pre[i-1]
-        } else {
-            ans[i] = suf[i+1] * pre[i-1]
-        }
+    
+    let suf = 1;
+    
+    for(let i = n-1; i >= 0; i--){
+        ans[i] *= suf;
+        suf *= nums[i];
     }
     return ans
 };

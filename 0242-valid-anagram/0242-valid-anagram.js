9d9b8f033f21c4a066ec603866c20fc4f453a7a7
Gino <43919172+gino23odar@users.noreply.github.com>
2024-08-09 21:11:00 -0500

    Time: 55 ms (96.56%), Space: 51.8 MB (66.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,19 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split('').sort().join('');
-    t = t.split('').sort().join('')
+    if(s.length != t.length) return false
+    let map = new Map()
     
-    return s == t
+    for(let i = 0; i < s.length; i++){
+        map.set(s[i], map.get(s[i]) + 1 || 1);
+    }
+    
+    for(let i = 0; i < t.length; i++){
+        if(!map.get(t[i])){
+            return false
+        } else {
+            map.set(t[i], map.get(t[i]) - 1)
+        }
+    }
+    return true
 };

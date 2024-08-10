8ec0596f47fca537196dc2258f15ae85fca003c9
Gino <43919172+gino23odar@users.noreply.github.com>
2024-08-10 12:22:25 -0500

    Time: 51 ms (93.04%), Space: 52.5 MB (40.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,9 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    if(nums.length < 3) return nums[0]
+    let n = Math.floor(nums.length/2)
     let map = new Map();
+    
     for(let i = 0; i < nums.length; i++){
-        map.set(nums[i], map.get(nums[i])+1|| 1)
-        if(map.get(nums[i]) > nums.length/2) return nums[i]
+        if(map.get(nums[i])){
+            if(map.get(nums[i]) == n){
+                return nums[i]
+            }
+            map.set(nums[i], map.get(nums[i])+1)
+        } else {
+            map.set(nums[i], 1)
+        }
     }
 };

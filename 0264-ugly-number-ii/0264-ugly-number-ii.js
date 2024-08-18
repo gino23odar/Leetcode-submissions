d6e71906570b80ed5981dbc8063ed32e91546c2d
Gino <43919172+gino23odar@users.noreply.github.com>
2024-08-18 17:41:36 -0500

    Time: 81 ms (25.20%), Space: 56.6 MB (22.05%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+    const factors = [2, 3, 5];
+    const offset = [0, 0, 0];
+    const ugly = [1];
+    n--;
+    
+    while(n--) {
+        const candidates = factors.map((val, i) => ugly[offset[i]] * val);
+        const next = Math.min(...candidates);
+        candidates.forEach((val, i) => candidates[i] === next ? offset[i]++ : null);
+        ugly.push(next);
+    }
+    return ugly.pop();
+};

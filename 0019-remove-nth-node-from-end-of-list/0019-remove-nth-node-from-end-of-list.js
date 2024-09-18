b27fa0ae679d045b19d39a7a59ad362c0548c012
Gino <43919172+gino23odar@users.noreply.github.com>
2024-09-18 16:22:09 -0500

    Time: 54 ms (68.54%), Space: 50.5 MB (53.91%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,10 +11,34 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let fast = head, slow = head;
-    for(let i = 0; i < n; i++) fast = fast.next;
-    if(!fast) return head.next;
-    while(fast.next) fast = fast.next, slow = slow.next;
-    slow.next = slow.next.next;
+    let temp1 = head ; 
+    let size = 0;
+    
+    while(temp1){
+       size ++;
+       temp1 = temp1.next;
+    }
+    if(size<n) return head;
+    if(size===1 && n===1){
+        head = null;
+        return head;
+    }
+
+    let pos = size - n ; 
+    if(pos===0){
+        let temp3=head;
+        head=head.next;
+        temp3.next=null;
+        return head;
+    }
+    let temp2 = head;
+
+    for(let i=1 ;i<=pos-1; i++ ){
+        temp2 = temp2.next;
+    }
+    temp1 = temp2.next.next;
+    temp2.next.next=null;
+    temp2.next = temp1;
+
     return head;
 };

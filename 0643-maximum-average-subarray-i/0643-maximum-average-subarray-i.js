0cb3c6903c70402f51c98cbb6565e9718607fc31
Gino <43919172+gino23odar@users.noreply.github.com>
2024-11-05 23:57:02 -0500

    Time: 2462 ms (5.00%), Space: 75.1 MB (5.04%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let maxAvg = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+    
+    for(let i = 0; i < k; i++){
+        sum += nums[i];
+    }
+    maxAvg = sum/k;
+    
+    for(let i = 0; i < nums.length-k; i++){
+        sum = sum - nums[i] + nums[i+k];
+        console.log(sum);
+        console.log(maxAvg);
+        if(sum/k > maxAvg) maxAvg = sum/k;
+    }
+    return maxAvg    
+};

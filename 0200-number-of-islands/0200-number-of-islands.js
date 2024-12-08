867fe64d17152f0de9e0a0ec8f641f3d2b6f16a1
Gino <43919172+gino23odar@users.noreply.github.com>
2024-12-08 13:05:16 -0500

    Time: 72 ms (63.66%), Space: 51.9 MB (84.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,21 +3,27 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let count = 0;
-    const mark = (row, col) =>{
-        grid[row][col] = 0;
-        if(grid[row+1] && grid[row+1][col] == 1) mark(row+1,col);
-        if(grid[row-1] && grid[row-1][col] == 1) mark(row-1, col);
-        if(grid[row][col+1] == 1) mark(row, col+1);
-        if(grid[row][col-1] == 1) mark(row, col-1);
+    let counter = 0;
+    
+    const dfs = (grid, r, c) =>{
+        let nr = grid.length;
+        let nc = grid[0].length;
+        
+        if(r >= nr || r < 0 || c >= nc || c < 0 || grid[r][c] == '0') return;
+        
+        grid[r][c] = '0';
+        
+        dfs(grid,r+1,c);
+        dfs(grid,r-1,c);
+        dfs(grid,r,c+1);
+        dfs(grid,r,c-1);
     }
+    
     for(let i = 0; i < grid.length; i++){
-        for(let j = 0; j < grid[i].length; j++){
-            if(grid[i][j] == 1){
-                count++;
-                mark(i,j)
-            }
+        for(let j = 0; j < grid[0].length; j++){
+            if(grid[i][j] == '1') counter++;
+            dfs(grid, i, j);
         }
     }
-    return count
+    return counter
 };

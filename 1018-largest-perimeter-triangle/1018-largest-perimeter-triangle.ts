e93851df8a9356677f2378606a58465c877de620
Gino <43919172+gino23odar@users.noreply.github.com>
2025-09-27 23:18:51 -0500

    Time: 22 ms (94.64%), Space: 57.9 MB (87.5%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.ts
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.ts
@@ -1,6 +1,5 @@
 function largestPerimeter(nums: number[]): number {
     nums.sort((a,b) => a - b);
-    console.log(nums);
     for(let i = nums.length-3; i >= 0; i--){
         if(nums[i] + nums[i+1] > nums[i+2]){
             return nums[i] + nums[i+1] + nums[i+2];
